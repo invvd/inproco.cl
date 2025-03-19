2a295b209d2617683c6e631808718eb48f76db21
Danny <danymassmcf@gmail.com>
2025-03-19 20:51:33 -0300

    exported Nosotros section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./sections/Hero";
+import Nosotros from "./sections/Nosotros";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="aspect-[4/3] relative w-screen max-h-screen top-0 z-0">
-        <video
-          controlsList="nodownload"
-          loop
-          className="opacity-75 -z-10 absolute top-0 size-full object-cover"
-          autoPlay
-          muted
-          playsInline
-        >
-          <source src="/videos/bg.mp4" />
-        </video>
-      </section>
+      <Hero videoSrc="/videos/bg.mp4" />
+      <Nosotros />
     </>
   );
 }
