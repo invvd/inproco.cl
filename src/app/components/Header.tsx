5403834dd563be588f599eacda67e8aac619524f
Danny <danymassmcf@gmail.com>
2025-03-20 00:58:53 -0300

    added hover animation to logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -34,7 +34,7 @@ export function Header() {
   return (
     <>
       <header className="z-10 backdrop-brightness-75 flex fixed w-auto h-16 justify-between items-center p-4 rounded-lg top-2 left-2 right-2 max-w-[calc(100%-1rem)] backdrop-blur-sm">
-        <Link href="/">
+        <Link className="hover:scale-105 transition" href="/">
           <Image
             draggable={false}
             width={100}
