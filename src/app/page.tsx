6bb2abf36c40354136e2df4cbe3dcf20a3a06270
Danny <danymassmcf@gmail.com>
2025-03-20 02:21:08 -0300

    added proyectos section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./sections/Hero";
 import Nosotros from "./sections/Nosotros";
+import Proyectos from "./sections/Proyectos";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero videoSrc="/videos/bg.mp4" />
       <Nosotros />
+      <Proyectos />
     </>
   );
 }
