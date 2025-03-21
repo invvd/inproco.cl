53258efff937fabf63a15c11cc19f40ce3f4a383
Danny <danymassmcf@gmail.com>
2025-03-20 21:00:43 -0300

    moved sections into components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
-import Hero from "./sections/Hero";
-import Nosotros from "./sections/Nosotros";
-import Proyectos from "./sections/Proyectos";
+import Hero from "./components/sections/Hero";
+import Nosotros from "./components/sections/Nosotros";
+import Proyectos from "./components/sections/Proyectos";
 
 export default function Home() {
   return (
