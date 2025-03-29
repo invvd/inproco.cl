35ca71f6517f24b6bd92c43892427e393812c268
Danny <danymassmcf@gmail.com>
2025-03-29 02:08:22 -0300

    moved files

--- src/app/components/sections/Contacto.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// Correo, formulario, mapa, redes sociales (en detalle)
-function Contacto() {
-  return <div>Contacto</div>;
-}
-
-export default Contacto;
--- src/app/components/sections/Servicios.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Servicios() {
-  return <div>Servicios</div>;
-}
-
-export default Servicios;
