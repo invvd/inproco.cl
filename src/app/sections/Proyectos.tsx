a4aa2266b1cdf43d0030b3e3873adfe7097c077e
Danny <danymassmcf@gmail.com>
2025-06-17 21:52:11 -0400

    adjusted lil things

--- src/app/sections/Proyectos.tsx
+++ src/app/sections/Proyectos.tsx
@@ -12,38 +12,29 @@ function Proyectos() {
   return (
     <section
       id="proyectos"
-      className="text-neutral-900 bg-gradient-to-br  from-white pt-10 to-neutral-400 flex flex-col relative items-center"
+      className="text-neutral-900 bg-neutral-200 flex px-10 lg:px-20 py-20 flex-col relative items-center"
     >
       <div className="flex justify-center items-center gap-2">
         <Image
           draggable={false}
           loading="lazy"
           alt="Insignia de Inproco"
-          className="drop-shadow-md -translate-y-0.5 w-10 sm:w-12 invert"
+          className="drop-shadow-md -translate-y-0.5 object-cover h-full invert"
           src={"/logos/badge-white.webp"}
           width={50}
           height={50}
         />
         <h2
-          className={`${monument.className} font-bold uppercase text-center text-4xl xs:text-5x z-10l`}
+          className={`${monument.className} font-bold uppercase text-center text-3xl md:text-4xl lg:text-5xl z-10`}
         >
-          Proyectos
+          Nosotros
         </h2>
       </div>
-      <p className="text-center mt-2 max-w-[80vw]">
-        Descubre nuestros proyectos y enamórate de cada detalle! 🏗️✨
+      <p className="text-center mb-10 max-w-[80vw]">
+        Descubre nuestros proyectos y enamórate de cada detalle!
       </p>
-      {/* <video
-        src="/videos/proyectos-bg.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-        className="absolute top-0 left-0 w-full h-full object-cover opacity-150 grayscale contrast-150"
-      /> */}
       <div
-        className={`p-10 w-full max-w-[1050px] grid grid-flow-row grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-5 [&>div]:rounded-sm min-h-80 [&>div]:h-52 [&>div]:overflow-hidden [&>div]:shadow-md text-white/90`}
+        className={`w-full max-w-[1000px] grid grid-flow-row grid-cols-1 md:grid-cols-6 md:grid-rows-2 gap-5 [&>div]:rounded-sm min-h-80 [&>div]:h-52 [&>div]:overflow-hidden [&>div]:shadow-md text-white/90`}
       >
         <div>
           <Link
@@ -54,7 +45,7 @@ function Proyectos() {
             <Image
               loading="lazy"
               alt=""
-              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[15%] transition duration-300 group-hover:blur-xs"
+              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[30%] transition duration-300 group-hover:blur-xs"
               src={"/images/integracion.jpg"}
               width={500}
               height={500}
@@ -66,7 +57,7 @@ function Proyectos() {
             </h3>
           </Link>
         </div>
-        <div className="md:col-span-2">
+        <div className="md:col-span-4">
           <Link
             target="_blank"
             href="/"
@@ -75,7 +66,7 @@ function Proyectos() {
             <Image
               loading="lazy"
               alt=""
-              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[15%] transition duration-300 group-hover:blur-xs"
+              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[30%] transition duration-300 group-hover:blur-xs"
               src={"/images/ingenieria.jpg"}
               width={500}
               height={500}
@@ -87,7 +78,7 @@ function Proyectos() {
             </h3>
           </Link>
         </div>
-        <div className="md:col-span-2 md:row-start-2">
+        <div className="md:col-span-4 md:row-start-2">
           <Link
             target="_blank"
             href="/"
@@ -96,7 +87,7 @@ function Proyectos() {
             <Image
               loading="lazy"
               alt=""
-              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[15%] transition duration-300 group-hover:blur-xs"
+              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[30%] transition duration-300 group-hover:blur-xs"
               src={"/images/mecanica.jpg"}
               width={500}
               height={500}
@@ -116,7 +107,7 @@ function Proyectos() {
           >
             <Image
               alt=""
-              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[15%] transition duration-300 group-hover:blur-xs"
+              className="group-hover:scale-105 size-full absolute object-cover opacity-60 group-hover:opacity-[30%] transition duration-300 group-hover:blur-xs"
               src={"/images/terreno.jpg"}
               width={500}
               height={500}
